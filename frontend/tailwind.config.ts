55dc876fda137739559fe6b4539356bb2c16f080
ChristianRukundo <siboruremarukundoc1@gmail.com>
2025-07-27 19:17:03 +0200

    tailwind config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss"
 const config = {
   darkMode: "class",
   content: [
@@ -75,6 +75,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-};
+} satisfies Config;
 
 export default config;
